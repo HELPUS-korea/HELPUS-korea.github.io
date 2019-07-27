a1521f03551a7f06c2609eeecfa60d7f32e863d2
HaeJun Seo <kan02134@gmail.com>
2019-07-28 00:06:21 +0900

    Chore: Add vue-icon module

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import icon from 'vue-icon';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.use(icon, 'v-icons');
 
 new Vue({
   router,
